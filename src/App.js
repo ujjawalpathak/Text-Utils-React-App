d123e8bbb34a02de61522c67b2d781a62e95364c
ujjawalpathak <ujjawal.pathak2007@gmail.com>
2023-11-21 04:10:10 -0500

    added alert using states

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Alerts from './components/Alerts';
 // import About from './components/About ';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
@@ -8,20 +9,31 @@ import React, {useState} from 'react';
 function App() {
 
   const [displayMode, setDisplayMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg : message,
+      type : type
+    })
+  }
 
   const toggleMode = ()=>{
     if(displayMode === "light"){
       setDisplayMode("dark");
       document.body.style.backgroundColor = "#042743";
+      showAlert("Dark Mode is enabled.", "success");
     }else{
       setDisplayMode("light");
       document.body.style.backgroundColor = "white";
+      showAlert("Light Mode is enabled.", "success");
     }
   }
 
   return (
     <>
       <Navbar title="TextUtils" displayMode={displayMode} toggleMode={toggleMode} homepage="Home" aboutpage="About"/>
+      <Alerts alert={alert}/>
       <div className='container my-3'>
       <TextForm heading="Enter Your Text Below" displayMode={displayMode}/>
       {/* <About/> */}
