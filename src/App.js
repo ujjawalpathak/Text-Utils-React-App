f1585874b73d1a20b789fcc0f0630fac79ce9fbf
ujjawalpathak <ujjawal.pathak2007@gmail.com>
2023-11-20 21:13:24 -0500

    adding about us component and backgorund color using states

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import About from './components/About ';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
@@ -9,6 +10,7 @@ function App() {
       <Navbar title="TextUtils" homepage="Home" aboutpage="About"/>
       <div className='container my-3'>
       <TextForm heading="Enter Your Text Below"/>
+      <About/>
       </div>
     </>
   );
