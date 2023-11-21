d19c549b42d7cd1b7cb779319d999228b1eb740c
ujjawalpathak <ujjawal.pathak2007@gmail.com>
2023-11-20 20:38:58 -0500

    adding clear button and resetting default text on focus

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react'
 
 export default function TextForm(props) {
+
     const handleOnChange = (event) => {
         console.log("on change");
         setText(event.target.value);
+    }
+
+    const resetDefault = (e) => {
+        e.target.value = "";
+        setText(e.target.value);
     }
 
     const handleUppercaseClick = () => {
@@ -13,9 +19,15 @@ export default function TextForm(props) {
     }
 
     const handleLowercaseClick = () => {
-        console.log("UpperCase function");
+        console.log("Lowercase function");
         let lowerText = text.toLowerCase();
         setText(lowerText);
+    }
+
+    const handleClearTextClick = () => {
+        console.log("ClearText function");
+        let clearText = "";
+        setText(clearText);
     }
 
     const [text, setText] = useState("Enter your text here");
@@ -25,10 +37,11 @@ export default function TextForm(props) {
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
                     <label htmlFor="textBox" className="form-label">{props.details}</label>
-                    <textarea className="form-control" id="textBox" value={text} onChange={handleOnChange} rows="8"></textarea>
+                    <textarea className="form-control" id="textBox" value={text} onFocus={resetDefault} onChange={handleOnChange} rows="8"></textarea>
                 </div>
                 <button className='btn btn-primary mx-2' onClick={handleUppercaseClick}>Convert to Upper Case</button>
                 <button className='btn btn-primary mx-2' onClick={handleLowercaseClick}>Convert to Lower Case</button>
+                <button className='btn btn-primary mx-2' onClick={handleClearTextClick}>Clear Text</button>
             </div>
             <div className='container my-3'>
                 <h3>Your Text Summary</h3>
