16b15705c195e8be784423ac80605dcbda17f894
ujjawalpathak <ujjawal.pathak2007@gmail.com>
2023-11-24 22:29:55 -0500

    added react router dom for about page

--- src/App.js
+++ src/App.js
@@ -1,56 +1,54 @@
 
 import './App.css';
 import Alerts from './components/Alerts';
-// import About from './components/About ';
+import About from './components/About ';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
 
   const [displayMode, setDisplayMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
-      msg : message,
-      type : type
+      msg: message,
+      type: type
     });
-    setTimeout( ()=> {
+    setTimeout(() => {
       setAlert(null)
     }, 1500);
   }
 
-  const toggleMode = ()=>{
-    if(displayMode === "light"){
+  const toggleMode = () => {
+    if (displayMode === "light") {
       setDisplayMode("dark");
       document.body.style.backgroundColor = "#042743";
       showAlert("Dark Mode is enabled.", "success");
-      // document.title = "TextUtils - Dark Mode";
-      // setInterval(() => {
-      //   document.title = "Dark Mode Enabled";
-      // }, 2000);
-      // setInterval(() => {
-      //   document.title = "Install TextUtils";
-      // }, 2000);
-    }else{
+    } else {
       setDisplayMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light Mode is enabled.", "success");
-      // document.title = "TextUtils - Light Mode";  
     }
   }
 
   return (
     <>
-      <Navbar title="TextUtilss" displayMode={displayMode} toggleMode={toggleMode} homepage="Home" aboutpage="About"/>
-      <Alerts alert={alert}/>
-      <div className='container my-3'>
-      <TextForm heading="Enter Your Text Below" displayMode={displayMode}/>
-      {/* <About/> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" displayMode={displayMode} toggleMode={toggleMode} homepage="Home" aboutpage="About" />
+        <Alerts alert={alert} />
+        <div className='container my-3'>
+          <Routes>
+          <Route exact path="/" element={<TextForm heading="Enter Your Text Below" displayMode={displayMode} />} />
+          <Route exact path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
 
 export default App;
+
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.displayMode} bg-${props.displayMode} border-bottom border-body data-bs-theme=${props.displayMode}`}>
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">{props.title}</a>
+                <Link className="navbar-brand" href="/">{props.title}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">{props.homepage}</a>
+                            <Link className="nav-link active" aria-current="page" to="/">{props.homepage}</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">{props.aboutpage}</a>
+                            <Link className="nav-link" to="/about">{props.aboutpage}</Link>
                         </li>
                     </ul>
                     <form className="d-flex" role="search">
